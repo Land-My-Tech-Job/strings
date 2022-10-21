19188f91d1332c412207ca541bb6ddace6e493ef
LMTJ-Teacher <116300715+LMTJ-Teacher@users.noreply.github.com>
2022-10-20 20:49:48 -0400

    Create string.js

---
+++ string.js
@@ -0,0 +1,47 @@
+// Problem One
+
+//Write a JavaScript function isBlank(input) that checks whether or not the input String is blank.
+
+// The function returns true if the input String is blank, false otherwise.
+
+// Problem Two
+
+// Write a JavaScript function stringToArray(input) to split a string and convert it into an array of words.
+
+// The function should return the array of words.
+
+// Problem Three
+
+// Write a JavaScript function abbreviateName(name) to convert a string (a person’s first and last names) to abbreviated form. If there is only a first name listed, then return just that name.
+
+// Problem Four
+
+// Write a JavaScript function titleCase(input) to convert an input string to title case. Note that titleCase has the first letter of each word capitalized and every other letter lowercase. Note in the example below that “JavaScript” becomes “Javascript” for the final result.
+
+// Problem Five
+
+// Write a JavaScript function firstN(array, n) to get the first n elements of an array. 
+
+// If n is not given, get the first element.
+// If n < 0, return the empty array.
+// If n > number of elements in the array, return the entire array
+
+// Problem Six
+
+// Write a JavaScript function lastN(array, n) to get the last n element of an array. 
+
+// If n is not given, return the last element.
+// If n < 0, return the empty array.
+// If n >  number of elements in the array, return the entire array.
+
+// Problem Seven
+
+// Write a JavaScript function sumPair(numbers, target) to find the indices of a pair of consecutive elements from a given array whose sum equals a specific target number.
+
+// The function should return an array of the indices of the pair of consecutive elements or the array [-1, -1] if a pair is not found.
+
+// Problem Eight
+
+// Write a JavaScript function move(array, from, to) to move an array element from one position to another.
+
+// The function should return the changed array.
